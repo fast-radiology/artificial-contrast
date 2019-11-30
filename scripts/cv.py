f3db09a0f326604150c0a8ec382e74ee329cbfa6
jacek <rzeszutekjacek@gmail.com>
2019-11-30 20:20:24 +0100

    Adapt to multiple folds

--- scripts/cv.py
+++ scripts/cv.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import json
 
 from fast_radiology.seed import random_seed
 
@@ -12,26 +13,27 @@ import numpy as np
 import pandas as pd
 from fastai.vision import *
 from fastai.distributed import *
-from sklearn.model_selection import KFold
 
 from fast_radiology.metrics import dice as dice3D
-from artificial_contrast.dicom import open_dcm_image, open_dcm_mask
-from artificial_contrast.data import get_scans, get_patients, get_data
+from artificial_contrast.dicom import (
+    open_dcm_image_factory as simple_open_dcm_image_factory,
+    open_dcm_mask,
+)
+from artificial_contrast.data import get_scans, get_data
 from artificial_contrast.learner import get_learner
 
-fastai.vision.image.open_image = open_dcm_image
+
 fastai.vision.image.open_mask = open_dcm_mask
-fastai.vision.data.open_image = open_dcm_image
 fastai.vision.data.open_mask = open_dcm_mask
-open_image = open_dcm_image
 open_mask = open_dcm_mask
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--local_rank", type=int)
-args = parser.parse_args()
-torch.cuda.set_device(args.local_rank)
-torch.distributed.init_process_group(backend='nccl', init_method='env://')
+# To explore later
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--local_rank", type=int)
+# args = parser.parse_args()
+# torch.cuda.set_device(args.local_rank)
+# torch.distributed.init_process_group(backend='nccl', init_method='env://')
 
 
 # Config
@@ -39,23 +41,37 @@ torch.distributed.init_process_group(backend='nccl', init_method='env://')
 HOME_PATH = os.environ['HOME']
 DATA_PATH = os.environ['DATA']
 MODEL_SAVE_PATH = os.environ['MODEL_SAVE']
+FOLDS_PATH = os.environ['FOLDS']
 data_path = Path(DATA_PATH)
+
+EXPERIMENT_NAME = os.envrion['EXPERIMENT_NAME']
 
 IMG_SIZE = 512
 BS = 20
 
 
+DCM_LOAD_FUNC = {
+    'simple_window_-100_300': simple_open_dcm_image_factory,
+    'simple_multiple_windows': simple_open_dcm_image_factory,
+    # 'freqs_no_limit_window': freqs_open_dcm_image_factory,
+    # 'freqs_window_-100_300': freqs_open_dcm_image_factory,
+}
+
+
 # CV
-N_FOLDS = 10
 
 scans = get_scans(data_path)
-patients = get_patients(data_path)
-print('Number of patients: ', len(patients))
+folds_df = pd.DataFrame(FOLDS_PATH)
 
-kfold = KFold(N_FOLDS, shuffle=True, random_state=SEED)
+for idx, fold in folds_df.iterrows():
+    conf = json.loads(fold[EXPERIMENT_NAME])
+    open_dcm_image_func = DCM_LOAD_FUNC(conf)
 
-for train_index, val_index in kfold.split(patients):
-    validation_patients = list(np.array(patients)[val_index])
+    fastai.vision.image.open_image = open_dcm_image_func
+    fastai.vision.data.open_image = open_dcm_image_func
+    open_image = open_dcm_image_func
+
+    validation_patients = fold['validation_patients']
     print('Validation patients: ', validation_patients)
 
     data = get_data(scans, HOME_PATH, validation_patients, bs=BS)
@@ -98,7 +114,7 @@ for train_index, val_index in kfold.split(patients):
 
         patient_dice = dice3D(val_pred_3d, val_target_3d)
 
-        fold_results.append({'patient': val_patient, 'dice': patient_dice})
+        fold_results.append({'patient': val_patient, 'dice': patient_dice.item()})
 
     fold_results_df = pd.DataFrame(fold_results)
     print(fold_results_df)
