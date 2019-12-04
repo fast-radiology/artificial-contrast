c8e9e9217f49239774fbe1532e70be4a5c5dfe37
Mateusz Flieger <mateuszflieger@gmail.com>
2019-12-04 19:16:12 +0100

    Dry coded standard mean,std approach

--- scripts/generate_experiments.py
+++ scripts/generate_experiments.py
@@ -10,8 +10,12 @@ import torch
 # monkey patch Path from pathlib
 from fastai.vision import *
 
-from artificial_contrast.data import get_scans, get_patients
-from artificial_contrast.dicom import read_HU_array
+from artificial_contrast.data import get_scans, get_patients, get_y_fn
+from artificial_contrast.dicom import (
+    read_HU_array,
+    open_dcm_image_factory,
+    open_dcm_mask,
+)
 from artificial_contrast.freqs import (
     freqhist_bins,
     hist_scaled_img,
@@ -24,6 +28,8 @@ from artificial_contrast.const import (
     FREQS_NO_LIMIT_WINDOWS,
     FREQS,
     NORM_STATS,
+    SIMPLE_MULTIPLE_WINDOWS,
+    SIMPLE_WINDOW_SMALL,
     TRAIN_PATIENTS,
     VALIDATION_PATIENTS,
     WINDOWS,
@@ -34,6 +40,7 @@ from sklearn.model_selection import KFold
 
 N_FOLDS = 10
 STANDARD_WINDOWS = [-100, 300]
+EXTENDED_WINDOWS = [[-40, 120], [-100, 300], [300, 2000]]
 DATA_PATH = os.environ['DATA']
 
 data_path = Path(DATA_PATH)
@@ -64,6 +71,31 @@ def get_freqs_method_dict(scans: List[str], windows: List[int]) -> Dict[str, obj
     return method_result
 
 
+def get_standard_method_dict(scans: List[str], windows: List[int]) -> Dict[str, object]:
+    scans_with_not_empty_mask = []
+    for scan in scans:
+        mask = open_dcm_mask(get_y_fn(scan))
+        if mask.sum() > 0:
+            scans_with_not_empty_mask.append(scan)
+
+    sums = []
+    for scan in scans_with_not_empty_mask:
+        sums.append(open_dcm_image(scan).sum(axis=(1, 2)))
+
+    stacked_sums = torch.stack(sums)
+    means = stacked_sums.sum(axis=0) / (512 * 512 * len(scans_with_not_empty_mask))
+
+    variances = []
+    for scan in scans_with_not_empty_mask:
+        variances.append(
+            ((open_dcm_image(scan) - means[..., None, None]) ** 2).sum(axis=(1, 2))
+        )
+
+    stds = torch.sqrt(1 / (sum(counts) - 1) * torch.stack(variances).sum(axis=(0)))
+
+    return {WINDOWS: windows, NORM_STATS: (means, stds)}
+
+
 folds = []
 kfold = KFold(N_FOLDS, shuffle=True, random_state=SEED)
 for train_index, val_index in kfold.split(patients):
@@ -79,9 +111,12 @@ for train_index, val_index in kfold.split(patients):
     scans = get_scans(data_path, patients=train_patients)
 
     # freqs
-    result[FREQS_NO_LIMIT_WINDOWS] = get_freqs_method_dict(scans, None)
-    result[FREQS_LIMIT_WINDOWS] = get_freqs_method_dict(scans, STANDARD_WINDOWS)
+    # result[FREQS_NO_LIMIT_WINDOWS] = get_freqs_method_dict(scans, None)
+    # result[FREQS_LIMIT_WINDOWS] = get_freqs_method_dict(scans, STANDARD_WINDOWS)
 
+    result[SIMPLE_WINDOW_SMALL] = get_standard_method_dict(scans, STANDARD_WINDOWS)
+
+    get_standard_method_dict
     folds.append(result)
     print(result)
 
