38720d646d5cbbec9e51561f85da956c51b1e8a2
jacek <rzeszutekjacek@gmail.com>
2019-12-08 19:18:04 +0100

    Add script to check sample size impact

---
+++ scripts/sample_size_effect.py
@@ -0,0 +1,83 @@
+import os
+import json
+from random import sample
+
+from fast_radiology.seed import random_seed
+from artificial_contrast.const import SEED
+
+random_seed(SEED)
+
+import torch
+import fastai
+import numpy as np
+import pandas as pd
+from fastai.vision import *
+from fastai.distributed import *
+from sklearn.model_selection import KFold
+
+from artificial_contrast.dicom import (
+    open_dcm_image_factory as simple_open_dcm_image_factory,
+    open_dcm_mask,
+)
+from artificial_contrast.data import get_scans, get_data, get_patients
+from artificial_contrast.learner import get_learner
+from artificial_contrast.const import WINDOWS
+from artificial_contrast.evaluate import evaluate_patients
+
+fastai.vision.image.open_mask = open_dcm_mask
+fastai.vision.data.open_mask = open_dcm_mask
+open_mask = open_dcm_mask
+
+# Config
+
+HOME_PATH = os.environ['HOME']
+DATA_PATH = os.environ['DATA']
+MODEL_SAVE_PATH = os.environ['MODEL_SAVE']
+
+data_path = Path(DATA_PATH)
+
+# Do we only run it for simple load?
+# What do we put into normalization?
+DCM_CONF = {WINDOWS: [[-100, 300], [-100, 300], [-100, 300]], NORM_STATS: None}
+
+IMG_SIZE = 512
+BS = 20
+
+SAMPLE_SIZES = [30, 60, 90, 120]
+SAMPLING_ROUNDS = 5
+PER_SAMPLE_FOLDS = 5
+
+
+# EXPERIMENT
+open_dcm_image_func = simple_open_dcm_image_factory(DCM_CONF)
+fastai.vision.image.open_image = open_dcm_image_func
+fastai.vision.data.open_image = open_dcm_image_func
+open_image = open_dcm_image_func
+
+
+patients = get_patients(data_path)
+
+for sample_size in SAMPLE_SIZES:
+    print(f'Sample size: {sample_size}')
+
+    # Or maybe do K-fold here too..
+    for i in range(SAMPLING_ROUNDS):
+
+        sampled_patients = sample(patients, sample_size)
+        print(f'{i} sampled patients: {sampled_patients}')
+        scans = get_scans(data_path, patients=sampled_patients)
+
+        kfold = KFold(PER_SAMPLE_FOLDS, shuffle=True, random_state=SEED)
+        for train_index, val_index in kfold.split(sampled_patients):
+            validation_patients = sampled_patients[val_index]
+            print('Validation patients: ', validation_patients)
+
+            data = get_data(scans, HOME_PATH, validation_patients, bs=BS)
+            learn = get_learner(data, metrics=[dice], model_save_path=MODEL_SAVE_PATH)
+
+            learn.fit_one_cycle(10, 1e-4)
+
+            eval_df = evaluate_patients(learn, validation_patients, IMG_SIZE)
+            print(eval_df)
+            # TODO: aggregate all results into DF
+            print(f"mean: {eval_df[DICE_NAME].mean()}, std: {eval_df[DICE_NAME].std()}")
