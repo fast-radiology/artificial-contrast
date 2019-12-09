e3c652435fdd0694f0ba81fd1654c1e1cec78228
Mateusz Flieger <mateuszflieger@gmail.com>
2019-12-09 21:19:56 +0100

    Format

--- scripts/generate_experiments.py
+++ scripts/generate_experiments.py
@@ -116,11 +116,17 @@ for train_index, val_index in kfold.split(patients):
 
     # freqs
     result[FREQS_NO_LIMIT_WINDOWS] = json.dumps(get_freqs_method_dict(scans, None))
-    result[FREQS_LIMIT_WINDOWS] = json.dumps(get_freqs_method_dict(scans, STANDARD_WINDOWS[0]))
+    result[FREQS_LIMIT_WINDOWS] = json.dumps(
+        get_freqs_method_dict(scans, STANDARD_WINDOWS[0])
+    )
 
     # simple
-    result[SIMPLE_WINDOW_SMALL] = json.dumps(get_standard_method_dict(scans, STANDARD_WINDOWS))
-    result[SIMPLE_MULTIPLE_WINDOWS] = json.dumps(get_standard_method_dict(scans, EXTENDED_WINDOWS))
+    result[SIMPLE_WINDOW_SMALL] = json.dumps(
+        get_standard_method_dict(scans, STANDARD_WINDOWS)
+    )
+    result[SIMPLE_MULTIPLE_WINDOWS] = json.dumps(
+        get_standard_method_dict(scans, EXTENDED_WINDOWS)
+    )
 
     folds.append(result)
     print(result)
